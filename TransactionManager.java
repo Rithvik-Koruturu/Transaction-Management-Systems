069ede1e296d29fddfbd1ecc6260f5ff27bb6793
Avishi310 <avishigupta310@gmail.com>
2024-10-23 05:07:55 +0530

    Add files via upload

--- TransactionManager.java
+++ TransactionManager.java
@@ -1,6 +1,28 @@
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class TransactionManager {
-    // You can add transaction management logic here if needed
+
+    // This method starts a new transaction.
+    @Transactional
+    public void begin() {
+        // Logic to begin a transaction. This is typically handled by the Spring framework.
+        // You could use platform-specific transaction management here if needed.
+        System.out.println("Transaction started");
+    }
+
+    // This method commits the current transaction.
+    @Transactional
+    public void commit() {
+        // Logic to commit the transaction. In Spring, this is done automatically if no exceptions occur.
+        System.out.println("Transaction committed");
+    }
+
+    // This method rolls back the current transaction.
+    @Transactional
+    public void rollback() {
+        // Logic to rollback the transaction. This is done automatically in case of an exception.
+        System.out.println("Transaction rolled back");
+    }
 }
